208f31a739a1646b745890905d18713b073312d9
Dennis Campos <dennisjr89@gmail.com>
2021-05-09 20:34:08 -0400

    added login file and partials files

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 
 //main routes
 router.get("/", homeController.getIndex);
+router.get("/login", authController.getLogin);
 
 module.exports = router;
