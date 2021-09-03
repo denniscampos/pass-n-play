0c4fdf8c615f2065fbdcb34086295f04c1d331d4
Dennis Campos <dennisjr89@gmail.com>
2021-09-03 13:13:04 -0400

    removed socials form temporarily

--- views/partials/function.js
+++ views/partials/function.js
@@ -1,12 +1,6 @@
-const axios = require("axios");
-
 function math(a, b) {
   return a + b;
 }
-
-// async function addToFavorites(id, title, imgUrl) {
-//   return await axios.post("/mylists/", { id, title, imgUrl });
-// }
 
 module.exports = {
   math: math,
