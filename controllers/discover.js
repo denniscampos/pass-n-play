5523dea2086a07779b3814ba36e09a86f8cc3d58
Dennis Campos <dennisjr89@gmail.com>
2021-09-12 23:36:17 -0400

    added lodash for pagination

--- controllers/discover.js
+++ controllers/discover.js
@@ -1,22 +1,13 @@
 const axios = require("axios");
 const { request } = require("express");
+const _ = require("lodash");
 
 module.exports = {
   discover: async (req, res) => {
     try {
-      let gameUrl = `https://api.rawg.io/api/games?key=${process.env.API_GAME_KEY}&page=1&page_size=5`;
+      const page = _.get(req, "query.page", 1);
+      let gameUrl = `https://api.rawg.io/api/games?key=${process.env.API_GAME_KEY}&page=${page}&page_size=15`;
       let games = await axios.get(gameUrl);
-
-      // next page
-      gameUrl = games.data.next;
-      let newUrl = await axios.get(gameUrl);
-      let testMe = newUrl.data.results.map((game) => {
-        return {
-          id: game.id,
-          name: game.name,
-          image: game.background_image,
-        };
-      });
 
       const gameInfo = {
         games: games.data.results.map((game) => {
@@ -26,16 +17,11 @@ module.exports = {
             image: game.background_image,
           };
         }),
-        links: {
-          next: games.data.next,
-          prev: games.data.prev,
-        },
+        page: _.get(req, "query.page", 1),
       };
 
       res.render("discover", {
         gameInfo: gameInfo,
-        query: req.query,
-        testMe: testMe,
       });
     } catch (err) {
       console.log(err);
